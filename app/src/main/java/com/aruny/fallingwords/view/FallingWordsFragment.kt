251b9099c2649147d0f6ae75079295f6aadb6d23
Arun Yogeshwaran <arun.yogeshwaran@zenjob.com>
2023-04-14 00:41:24 +0200

    (The fragment is messy now!!)Adding two text views to show current score and lives

--- app/src/main/java/com/aruny/fallingwords/view/FallingWordsFragment.kt
+++ app/src/main/java/com/aruny/fallingwords/view/FallingWordsFragment.kt
@@ -19,6 +19,8 @@ import com.aruny.fallingwords.domain.UIWordsModel
 class FallingWordsFragment : Fragment() {
     private var listOfWords: List<UIWordsModel> = mutableListOf()
     private var currentWordIndex = 0
+    private var currentLives = MAX_LIVES
+    private var currentScore = 0
     private var _binding: FragmentFallingWordsBinding? = null
 
     // This property is only valid between onCreateView and
@@ -39,61 +41,86 @@ class FallingWordsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.textCurrentScore.text = getString(R.string.score_number, 0)
+        binding.textCurrentLives.text = getString(R.string.lives_number, MAX_LIVES)
 
         binding.buttonWrong.setOnClickListener {
             val currentWord = listOfWords[currentWordIndex]
-            // TODO: Update the score status here
             if (binding.textFallingWord.text != currentWord.correctTranslation) {
+                updateCurrentScore()
                 println("You are right bro!!")
             } else {
+                updateCurrentLives()
                 println("You are wrong bro!!")
             }
-//            findNavController().navigate(R.id.action_FallingWordsFragment_to_WelcomeFragment)
+            currentWordIndex++
+            startFallingWords()
         }
 
         binding.buttonCorrect.setOnClickListener {
-//            findNavController().navigate(R.id.action_FallingWordsFragment_to_GameResultFragment)
             val currentWord = listOfWords[currentWordIndex]
-            // TODO: Update the score status here
             if (binding.textFallingWord.text == currentWord.correctTranslation) {
+                updateCurrentScore()
                 println("You are right bro!!")
             } else {
+                updateCurrentLives()
                 println("You are wrong bro!!")
             }
+            currentWordIndex++
+            startFallingWords()
         }
 
-        val animation = AnimationUtils.loadAnimation(requireContext(), R.anim.anim_fall_down)
         viewModel.wordPairLiveData.observe(viewLifecycleOwner) {
             listOfWords = it
             binding.textFallingWord.text = listOfWords[currentWordIndex].optionWords.random()
-            animation.setAnimationListener(object : Animation.AnimationListener {
-                override fun onAnimationStart(animation: Animation?) {
-                    // TODO: Remove the Ans: here
-                    binding.textEnglishWord.text =
-                        listOfWords[currentWordIndex].englishWord + " (Ans:" + listOfWords[currentWordIndex].correctTranslation + ")"
-                    binding.textFallingWord.text =
-                        listOfWords[currentWordIndex].optionWords.random()
-                    binding.textFallingWord.startAnimation(animation)
-                }
-
-                override fun onAnimationEnd(animation: Animation?) {
-                    currentWordIndex++
-                    if (currentWordIndex < listOfWords.size) {
-                        binding.textFallingWord.startAnimation(animation)
-                    } else {
-                        findNavController().navigate(
-                            R.id.action_FallingWordsFragment_to_GameResultFragment
-                        )
-                    }
-                }
-
-                override fun onAnimationRepeat(animation: Animation?) {
-
-                }
-            })
-            binding.textFallingWord.startAnimation(animation)
+            startFallingWords()
         }
+
         viewModel.getWords()
+    }
+
+    private fun updateCurrentScore() {
+        binding.textCurrentScore.text = getString(R.string.score_number, ++currentScore)
+    }
+
+    private fun updateCurrentLives() {
+        if (--currentLives == 0) {
+            findNavController().navigate(R.id.action_FallingWordsFragment_to_GameResultFragment)
+        } else {
+            binding.textCurrentLives.text = getString(R.string.lives_number, currentLives)
+        }
+    }
+
+    private fun startFallingWords() {
+        val animation = AnimationUtils.loadAnimation(requireContext(), R.anim.anim_fall_down)
+        animation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+                // TODO: Remove the Ans: here
+                binding.textEnglishWord.text =
+                    listOfWords[currentWordIndex].englishWord + " (Ans:" + listOfWords[currentWordIndex].correctTranslation + ")"
+                binding.textFallingWord.text =
+                    listOfWords[currentWordIndex].optionWords.random()
+                binding.textFallingWord.startAnimation(animation)
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                updateCurrentLives()
+                currentWordIndex++
+                // Ensure we move to the result fragment when all the words are done
+                if (currentWordIndex < listOfWords.size) {
+                    binding.textFallingWord.startAnimation(animation)
+                } else {
+                    findNavController().navigate(
+                        R.id.action_FallingWordsFragment_to_GameResultFragment
+                    )
+                }
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+
+            }
+        })
+        binding.textFallingWord.startAnimation(animation)
     }
 
     override fun onDestroyView() {
@@ -101,4 +128,7 @@ class FallingWordsFragment : Fragment() {
         _binding = null
     }
 
+    companion object {
+        private const val MAX_LIVES = 3
+    }
 }
